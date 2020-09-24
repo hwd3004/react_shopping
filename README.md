03cfe81d2a30bfd16ec4c126cb87bf313cffcd0b
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-09-25 07:15:03 +0900

    2. 평화로운 쇼핑몰 레이아웃 디자인시간

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 0. 새로 시작
 
+---
+
 # 1. 부트스트랩 설치
 
 css 프레임워크
@@ -23,3 +25,56 @@ index.html에
 이후에 다시 https://getbootstrap.com/ 으로 가서
 docs에서 검색하거나, docs - components에서 태그 살펴보고 맘에 드는거 복붙
 원래는 https://react-bootstrap.github.io/ 에서 복붙해야하지만, 전자로 하는게 일단은 쉬움.
+
+---
+
+# 2. 평화로운 쇼핑몰 레이아웃 디자인시간
+
+https://react-bootstrap.github.io/ 입장
+Components 클릭
+검색창에 navbar 검색하면 나오는거 복붙
+
+복붙하면 에러뜸. 복붙한 컴포넌트를 상단에 import해야함.
+
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+
+
+대문 만들기
+https://react-bootstrap.github.io/ 에서 jumbotron 검색해서 복붙
+
+
+
+
+
+App.css에서
+
+.background {
+    background-image: url(/images/background.jpg);
+    background-position: center;
+    color: white;
+}
+
+리액트 특징으로, public 폴더 내부의 이미지를 쓰는 문법이다.
+src 폴더 등의 파일들은 리액트 앱을 발행했을 때 저절로 압축이 되는데,
+public 폴더는 리액트 앱을 발행했을 때, 사이트 루트경로에 그대로 남아있다.
+
+
+
+
+<div className="container">
+    <div className="row">
+        <div className="col-4"></div>
+        <div className="col-4"></div>
+        <div className="col-4"></div>
+    </div>
+</div>
+
+부트스트랩 문법
+row - 사이트를 세로로 12개의 칼럼으로 쪼갬.
+container - 좌우여백을 잡아줌.
+col-4 - 4개 칼럼 차지하는 div
+col-md-4 - 모바일, 가로폭이 좁은 화면에서 세로로 정렬
+
+
+https://react-bootstrap.netlify.app/layout/grid/
+여기서 레이아웃 문서 참고하면서 해야됨.
--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React from "react";
+import { Navbar,Nav,NavDropdown,Button,Jumbotron } from "react-bootstrap";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>ay</h1>
-      <button type="button" class="btn btn-primary">Primary</button>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">신발 쇼핑사이트</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* <Nav className="mr-auto"> */}
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Jumbotron className="background">
+        <h1>신규 오픈!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="/images/shoes1.jpg" width="100%"></img>
+            <h4>상품설명</h4>
+            <p> 상품설명 &amp; 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img src="/images/shoes2.jpg" width="100%"></img>
+            <h4>상품설명</h4>
+            <p> 상품설명 &amp; 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img src="/images/shoes3.jpg" width="100%"></img>
+            <h4>상품설명</h4>
+            <p> 상품설명 &amp; 가격</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
